9d5525833b667934aeb6c43cb645c816338c5f75
Youssef <youssef.samaan@mail.utoronto.ca>
2025-05-31 21:18:52 -0400

    Initial Project setup with Docker, Go, PostgreSQL

---
+++ cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/gorilla/mux" //registering the driver
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}).Methods("GET")
+
+	fmt.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+	dsn := "host=db port=5432 user=rescounts_user password=rescounts_pass dbname=rescounts_db sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Error with sql db :(")
+	}
+	defer db.Close()
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Brossef/rescounts-task
+
+go 1.24.3
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/stripe/stripe-go/v82 v82.2.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+)
